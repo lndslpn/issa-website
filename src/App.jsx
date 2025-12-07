233434388103bc67666ddabb59ecc27f88cdcb51
Landys Lepine <landys@ualberta.ca>
2025-12-07 17:35:26 -0500

    added transitions and scroll fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,117 @@
-import './App.css'
+import "./App.css";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
 
-import { Routes, Route } from 'react-router-dom'
+// Pages
+import Hero from "./components/Hero.jsx";
+import About from "./components/About.jsx";
+import Executives from "./components/Executives.jsx";
+import Events from "./components/Events.jsx";
+import Faq from "./components/FAQ.jsx";
+import Contact from "./components/Contact.jsx";
+import Resources from "./components/Resources.jsx";
 
-import Hero from './components/Hero.jsx'
-import About from './components/About.jsx'
-import Executives from './components/Executives.jsx'
-import Events from './components/Events.jsx'
-import Faq from './components/FAQ.jsx'
-import Contact from './components/Contact.jsx'
-import Resources from './components/Resources.jsx'
-import Navbar from './tools/Header.jsx'
-import Footer from './tools/Footer.jsx'
+// Layout
+import Navbar from "./tools/Header.jsx";
+import Footer from "./tools/Footer.jsx";
+
+// Animation helpers
+import PageTransition from "./components/PageTransition.jsx";
+import ScrollToTop from "./components/ScrollToTop.jsx";
 
 function App() {
+  const location = useLocation();
+
   return (
-    <div>
+    <div className="min-h-screen flex flex-col">
+      {/* Always visible */}
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Hero />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/executives" element={<Executives />} />
-        <Route path="/events" element={<Events />} />
-        <Route path="/faq" element={<Faq />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/resources" element={<Resources />} />
-      </Routes>
+
+      {/* Scroll to top when route changes */}
+      <ScrollToTop />
+
+      {/* Animated page container */}
+      <main className="flex-1">
+        <AnimatePresence mode="wait">
+          <Routes location={location} key={location.pathname}>
+            
+            {/* Home */}
+            <Route
+              path="/"
+              element={
+                <PageTransition>
+                  <Hero />
+                </PageTransition>
+              }
+            />
+
+            {/* About */}
+            <Route
+              path="/about"
+              element={
+                <PageTransition>
+                  <About />
+                </PageTransition>
+              }
+            />
+
+            {/* Executives */}
+            <Route
+              path="/executives"
+              element={
+                <PageTransition>
+                  <Executives />
+                </PageTransition>
+              }
+            />
+
+            {/* Events */}
+            <Route
+              path="/events"
+              element={
+                <PageTransition>
+                  <Events />
+                </PageTransition>
+              }
+            />
+
+            {/* FAQ */}
+            <Route
+              path="/faq"
+              element={
+                <PageTransition>
+                  <Faq />
+                </PageTransition>
+              }
+            />
+
+            {/* Contact */}
+            <Route
+              path="/contact"
+              element={
+                <PageTransition>
+                  <Contact />
+                </PageTransition>
+              }
+            />
+
+            {/* Resources */}
+            <Route
+              path="/resources"
+              element={
+                <PageTransition>
+                  <Resources />
+                </PageTransition>
+              }
+            />
+          </Routes>
+        </AnimatePresence>
+      </main>
+
+      {/* Always visible */}
       <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
